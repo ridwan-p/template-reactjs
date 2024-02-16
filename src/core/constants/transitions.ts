e28abc09f03948870bc0f8379444fc0939b6c540
pamungkas <ridwan.pamungks@gmail.com>
2024-02-16 22:13:09 +0700

    set transision

---
+++ src/core/constants/transitions.ts
@@ -0,0 +1,16 @@
+// https://github.com/twbs/bootstrap/blob/v5.3.2/scss/_variables.scss
+export const TransitionTimeouts = {
+  Base: 200, // $transition-base
+  Fade: 150, // $transition-fade
+  Collapse: 350, // $transition-collapse
+  Modal: 300, // $modal-transition
+  Carousel: 600, // $carousel-transition
+  Offcanvas: 300, // $offcanvas-transition
+};
+
+export const TransitionStates = {
+  ENTERING: 'entering',
+  ENTERED: 'entered',
+  EXITING: 'exiting',
+  EXITED: 'exited',
+}
